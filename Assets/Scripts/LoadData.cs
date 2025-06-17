dd2a443a795e556f4b86ce5c6a85d88af05f7508
Le Trong Chien <chiencon472002@gmail.com>
2025-06-17 10:27:18 +0700

    Init

---
+++ Assets/Scripts/LoadData.cs
@@ -0,0 +1,63 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.IO;
+#if UNITY_EDITOR
+using UnityEditor;
+#endif
+using UnityEngine;
+
+
+
+[System.Serializable]
+public class SongKeyEvent
+{
+    public int id;
+    public float time;
+    public int lane;
+    public int backgroundId;
+    public int backgroundIdDifferent;
+    public float duration;
+}
+
+[System.Serializable]
+public class NewSongKeyEvent
+{
+    public int id;
+    public float time;
+    public float duration;
+}
+
+
+
+[System.Serializable]
+public class SongData
+{
+    public List<SongKeyEvent> keys;
+}
+
+[System.Serializable]
+public class NewSongData
+{
+    public List<NewSongKeyEvent> keys;
+}
+
+
+public class LoadData : MonoBehaviour
+{
+    public static void SaveToAssets(SongData data, string fileName = "song.json")
+    {
+        string folderPath = Path.Combine(Application.dataPath, "Datas");
+        if (!Directory.Exists(folderPath))
+            Directory.CreateDirectory(folderPath);
+
+        string filePath = Path.Combine(folderPath, fileName);
+        string json = JsonUtility.ToJson(data, true); // pretty print
+
+        File.WriteAllText(filePath, json);
+        Debug.Log("Saved JSON to: " + filePath);
+
+#if UNITY_EDITOR
+        AssetDatabase.Refresh(); // Cập nhật để thấy file trong Project
+#endif
+    }
+}
